e2f1d369bf9db5304bd32ec92b8088344e380b33
Javier Pena <jpena@redhat.com>
2016-04-22 14:35:25 +0200

    Set proper file permissions for known_hosts file

--- manifests/worker.pp
+++ manifests/worker.pp
@@ -314,6 +314,11 @@ python setup.py develop",
       key     => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCcv42F0KURajhaHpXECtonyhyxyyIexl0eJvKCTnc6hCE2bf8Iymw/xQIxmIwoibFunSC74tZe2t7Zy+yf3nLeNgE3T8+79yNxA2N4cJuY1T51haE5T1LKTMEkPkA4ucS8Lvd7KiXeTWRqOUQtLDWiZSZxPILzlb13AQ1M2s4U3X0M7SBt4V27ezDe34OQbBHMAGVQOKZhQkNVp3e5gmMfPlE3FifjQ07RI2fyG8v/r4A8on9n/g8Ge0vbDyGR0Ejt314MJ9JpzQTSPzw05UkjJYE7Knw3sHyBU9qIFHEm1Gw4z0PukiuINUmnBDVkf9ep6IsIw4JSvzNQbaLO9t99',
       target  => "/home/${name}/.ssh/known_hosts",
       require => File["/home/${name}"],
+    } ->
+    file { "/home/${name}/.ssh/known_hosts":
+      mode    => '0644',
+      owner   => $name,
+      group   => $name,
     }
   }
 }
