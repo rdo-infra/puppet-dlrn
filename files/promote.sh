2dca2ec568585810c6576e50af7133586c26b2ea
David Moreau-Simard <dms@redhat.com>
2016-08-25 16:06:54 -0400

    Move promotions to functions, add remote promotion
    This will make it so the promote.sh script will promote a link
    on a remote instance using the same rsync configuration used by
    DLRN, if available.
    
    Change-Id: Ibf1b8fc2bea60c16217c1a8aad1bf58933cd360c

--- files/promote.sh
+++ files/promote.sh
@@ -1,4 +1,34 @@
 #!/bin/bash -xe
+function local_promote() {
+    # Promotes a symlink locally
+
+    # Verify uniqueness
+    count="$(find . -maxdepth 3 -mindepth 3 -type d -name \*${hash}\* | wc -l)"
+    if [ "${count}" != "1" ]; then
+        echo "Uniqueness must be enforced!"
+        exit 1
+    fi
+
+    ln -nsvf */*/*${hash}* ${linkname}
+    echo "${hash}" >> promote-${linkname}.log
+}
+
+function remote_promote() {
+    # Promotes a symlink remotely, if possible
+    config="/usr/local/share/dlrn/${instance}/projects.ini"
+
+    # Validate that the required configuration is available
+    [[ ! -f "${config}" ]] && return 0
+    grep -q "rsyncdest" "${config}" || return 0
+    grep -q "rsyncport" "${config}" || return 0
+
+    rsyncdest=$(grep "rsyncdest" "${config}" |cut -f2 -d =)
+    rsyncport=$(grep "rsyncport" "${config}" |cut -f2 -d =)
+
+    rsync -av -e "ssh -p ${port}" "${linkname}" "${rsyncdest}"
+    rsync -av -e "ssh -p ${port}" "promote-${linkname}.log" "${rsyncdest}"
+}
+
 # Ensure we don't allow any special characters into the script
 for ARG in $@ ; do
     if [[ ! $ARG =~ ^[a-zA-Z0-9_-]+$ ]] ; then
@@ -21,15 +51,7 @@ fi
 
 linkname="${3:-current-passed-ci}"
 
-cd /home/${instance}/data/repos
-
-# Verify uniqueness
-count="$(find . -maxdepth 3 -mindepth 3 -type d -name \*${hash}\* | wc -l)"
-if [ "${count}" != "1" ]; then
-    echo "Uniqueness must be enforced!"
-    exit 1
-fi
-
-# Promote symlink locally
-ln -nsvf */*/*${hash}* ${linkname}
-echo "${hash}" >> promote-${linkname}.log
+pushd /home/${instance}/data/repos
+local_promote
+remote_promote
+popd
