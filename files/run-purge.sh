dc58b31b4df7a6505ce118c3d1e5510fbc4bd1df
Javier Pena <jpena@redhat.com>
2016-12-07 12:40:04 +0100

    Add run-purge script
    Just like we have a run-dlrn.sh helper script, add a run-purge.sh
    one to simplify purge operations on workers. It provides a set
    of sane defaults (60 days retention, do not remove anything on the
    special symlinked directories), sends output to a log file and makes
    sure no other dlrn commands can be run at the same time by using
    the same lock file.
    
    This script can be used in the future in a cron job if needed.
    
    Change-Id: I0331f0b49d830e83726960158301dae715e65f9c

---
+++ files/run-purge.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+LOCK="/home/${USER}/dlrn.lock"
+set -e
+
+# if any arguments are given, assume console foreground execution
+ARG=$1
+
+exec 200>$LOCK
+if !  flock -n 200
+then
+    if [ -n "$ARG" ]
+    then
+        echo "DLRN ${USER} running, please try again after disabling it."
+    fi
+    exit 1
+fi
+
+if [ ! -d /home/${USER}/dlrn-logs ]
+then
+    mkdir -p /home/${USER}/dlrn-logs
+fi
+
+source ~/.venv/bin/activate
+if [ -n "$ARG" ]
+then
+    LOGFILE=/home/${USER}/dlrn-logs/dlrn-purge.console.$(date +%s).log
+    ARGUMENTS=""    
+else
+    LOGFILE=/home/${USER}/dlrn-logs/dlrn-purge.$(date +%s).log
+    ARGUMENTS="--older-than 60 -y --exclude-dirs /home/${USER}/data/repos/current,/home/${USER}/data/repos/consistent,/home/${USER}/data/repos/current-passed-ci,/home/${USER}/data/repos/current-tripleo"
+fi
+cd ~/dlrn
+
+set +e
+echo `date` "Starting DLRN-purge run." >> $LOGFILE
+dlrn-purge --config-file /usr/local/share/dlrn/${USER}/projects.ini "${ARGUMENTS}" "$@" 2>> $LOGFILE
+RET=$?
+echo `date` "DLRN-purge run complete." >> $LOGFILE
+
+if [ -n "$ARG" ]
+then
+    echo Arguments: "$@"
+    cat $LOGFILE
+fi
+exit $RET
