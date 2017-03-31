a1d0bb7b05fdeec2e06d2b22049283a870b82af8
Javier Pena <jpena@redhat.com>
2017-03-31 14:26:27 +0200

    Add support for a public rsync option for workers
    We want to be able to expose the repos created by a worker via a
    public rsync service, for example to mirror the trunk.rdo contents
    on the public OpenStack infrastructure.
    
    Change-Id: I00050aef6cd328379bfc1ee78961c6c84f829f76

--- spec/classes/dlrn_common_spec.rb
+++ spec/classes/dlrn_common_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 describe 'dlrn::common' do
 
     let :facts do
-    {   :osfamily                  => 'RedHat',
+    {   :os                        => { :family => 'RedHat' },
+        :osfamily                  => 'RedHat',
         :operatingsystem           => 'Fedora',
         :operatingsystemrelease    => '24',
         :operatingsystemmajrelease => '24',
@@ -63,6 +64,10 @@ describe 'dlrn::common' do
          :service => 'http',
          :zone    => 'public',
        )
+       is_expected.to contain_firewalld_service('Allow rsyncd').with(
+         :service => 'rsyncd',
+         :zone    => 'public',
+       )
        is_expected.to contain_firewalld_port('Allow custom SSH port').with(
          :port     => 3300,
          :zone     => 'public',
@@ -75,6 +80,12 @@ describe 'dlrn::common' do
          :service => 'https',
          :zone    => 'public',
        )
+      end
+
+      it 'configures the rsync server' do
+        is_expected.to contain_class('rsync::server').with(
+          :use_xinetd => false,
+        )
       end
     end
 
