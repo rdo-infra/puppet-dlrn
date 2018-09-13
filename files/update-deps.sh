9246c609f333d07b47dba83c37ca4a61e7a0b92c
Javier Pena <jpena@redhat.com>
2018-09-13 10:17:16 +0200

    Set master branch to Stein in update-deps.sh
    We have bootstrapped the Stein dependencies in [1], so we need to
    update the default branch.
    
    [1] - https://review.rdoproject.org/r/16198
    
    Change-Id: I10e42eb7d02fa69eb09c2f9f49839d99bd277f7b

--- files/update-deps.sh
+++ files/update-deps.sh
@@ -4,7 +4,7 @@
 
 set -e
 
-MASTER_TAG=${MASTER_TAG:-rocky}
+MASTER_TAG=${MASTER_TAG:-stein}
 RELEASE=${RELEASE:-${USER#centos-}}
 DEPS_DIR=${DEPS_DIR:-${HOME}/data/repos/deps/}
 LATEST_DEPS_DIR=${DEPS_DIR}/latest/
